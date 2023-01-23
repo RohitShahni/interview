5ec300e86dfd2787c076b9cad66d915ff37db01c
rohit <rohitshahni121@gmail.com>
2023-01-23 23:27:20 +0530

    initial commit

---
+++ Datastrucure/index.py
@@ -0,0 +1,22 @@
+
+#  january -2200
+#  febuary  -2350
+#   march -2600
+#   april  - 2100
+#   may  - 2190
+
+
+
+
+expense2=[]
+expense = [2200, 2350, 2600,2190]
+
+
+for x in expense:
+   expense2.append(x/80)
+else:
+  print("In Else Block")
+  print(expense)
+  print(expense2)
+  print(expense2[1]-expense2[0])
+# print(expense2.index('o'))
